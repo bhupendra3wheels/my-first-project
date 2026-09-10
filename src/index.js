043af84eccda88999996417d3f60ba3319c54b8b
3wheelsAI <hello@3wheels.ai>
2026-09-10 11:30:00 +0530

    feat: add greet and add functions

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return `Hello, ${name}! Welcome.`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { greet, add, multiply };
