a3e3f762abe8cdd980d0ec6229163843caf75c98
3wheelsAI <hello@3wheels.ai>
2026-09-14 11:45:00 +0530

    test: add calculator tests

---
+++ tests/calculator.test.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require('../src/features/calculator');
+
+console.log('add(2,3) =', add(2, 3));
+console.log('subtract(5,2) =', subtract(5, 2));
+console.log('multiply(4,3) =', multiply(4, 3));
+console.log('divide(10,2) =', divide(10, 2));
